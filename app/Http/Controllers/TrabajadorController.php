f951467d28790a650815a193d21ba2b52eaab68f
davidiqm <al061737@uacam.mx>
2020-11-22 19:52:25 -0600

    Api lista

---
+++ app/Http/Controllers/TrabajadorController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Trabajador;
+use Illuminate\Http\Request;
+
+class TrabajadorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Trabajador::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'usuario' => 'required',
+            'nombre' => 'required',
+            'oficio' => 'required',
+            'edad' => 'required',
+            'correo' => 'required'
+        ]);
+        
+        $request->merge([
+            'id_tipo'=>2
+        ]);
+
+        Trabajador::create($request->all());
+
+        return $request;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Trabajador  $trabajador
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trabajador $trabajador)
+    {
+        return $trabajador;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Trabajador  $trabajador
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trabajador $trabajador)
+    {
+        $trabajador->update($request->all());
+
+        return $trabajador;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Trabajador  $trabajador
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trabajador $trabajador)
+    {
+        $trabajador->delete();
+    }
+}
